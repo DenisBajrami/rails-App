bc51a74eed53b97ea0f34f96597f1eb6abd32338
Denis Bajrami <denis.sec773@gmail.com>
2019-01-04 10:41:34 +0100

    Finish layout and routes

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,4 @@
 
 ActiveRecord::Schema.define(version: 2018_12_31_140700) do
 
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
